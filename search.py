1071e75b3c526627346ffb5f575f25e858b7eed6
Benny Roesler <bennytroesler@gmail.com>
2020-01-24 13:31:35 -0700

    Final Product

--- search.py
+++ search.py
@@ -9,19 +9,46 @@ def search(path, keyword):
     for root, dirs, files in os.walk(path, topdown=False):
         for name in files:
             if keyword.lower() in name.lower():
-                print("file: ", os.path.join(root, name))
-                list.append(os.path.join(root, name))
+                list.append(os.path.join(root, name) + "\n\n")
     return list
-
 
 def main():
     try:
-        print("Please select a folder.")
-        directory = easygui.diropenbox("Select a folder to recursively search", program)
-        keyword = easygui.enterbox("Enter the keyword to search for, case insensitive", program)
-        results = search(directory, keyword)
+        while True:
+            # Directory input
+            directory = easygui.diropenbox("Select a folder to recursively search", program)
+            if directory is None:
+                if easygui.ccbox("No folder selected. Try again?", program):
+                    continue
+                else:
+                    exit()
+
+            # Keyword input
+            keyword = easygui.enterbox("Enter the keyword to search for, case insensitive", program)
+            if keyword is None:
+                if easygui.ccbox("No keyword entered. Try again?", program):
+                    continue
+                else:
+                    exit(0)
+
+            results = search(directory, keyword)
+
+            if results is None:
+                if easygui.ccbox("No Results found. Try again?", program):
+                    continue
+                else:
+                    exit(0)
+            header = "To save press OK\nResults: \n Directory: " + directory + "\n Keyword: " + keyword + "\n Number of Results: " + str(len(results))
+            if easygui.textbox(header, program, results):
+                easygui.filesavebox("Save Results", program, program + "_" + keyword, results)
+            else:
+                exit(0)
 
 
+
+
+    except SystemExit:
+        exit(0)
     except:
         easygui.exceptionbox("Error occurred. Please contact the maintainer of this program", program)
 
