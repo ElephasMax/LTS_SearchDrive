695620978dc730aa6a5e145f92bc08e524bfe58e
Benny Roesler <bennytroesler@gmail.com>
2020-01-27 15:16:39 -0700

    Finished

---
+++ README.md
@@ -0,0 +1,11 @@
+# DriveSearch
+> This program recursively searches a directory for files that contains a keyword.
+
+#Installing Requirements
+```
+pip install -r requirements.txt
+
+or
+
+pip3 install -r requirments.txt
+```
--- search.py
+++ search.py
@@ -9,8 +9,15 @@ def search(path, keyword):
     for root, dirs, files in os.walk(path, topdown=False):
         for name in files:
             if keyword.lower() in name.lower():
-                list.append(os.path.join(root, name) + "\n\n")
+                list.append(os.path.join(root, name))
     return list
+
+
+def save_file(path, text):
+    f = open(path+".txt", "a")
+    for line in text:
+        f.write(line+"\n")
+    exit(1)
 
 def main():
     try:
@@ -22,6 +29,7 @@ def main():
                     continue
                 else:
                     exit()
+            print("Directory: ", directory)
 
             # Keyword input
             keyword = easygui.enterbox("Enter the keyword to search for, case insensitive", program)
@@ -31,26 +39,36 @@ def main():
                 else:
                     exit(0)
 
+            print("Keyword: ", keyword)
+
             results = search(directory, keyword)
+
+            print("Entries: ", len(results))
+            print("Results: ", results)
 
             if results is None:
                 if easygui.ccbox("No Results found. Try again?", program):
                     continue
                 else:
                     exit(0)
-            header = "To save press OK\nResults: \n Directory: " + directory + "\n Keyword: " + keyword + "\n Number of Results: " + str(len(results))
-            if easygui.textbox(header, program, results):
-                easygui.filesavebox("Save Results", program, program + "_" + keyword, results)
+            header = "To save press OK.\nResults: \n Directory: " + directory + "\n Keyword: " + keyword + "\n Number of Results: " + str(len(results))
+            results_pretty = ""
+            for line in results:
+                results_pretty += line + "\n\n"
+            if easygui.textbox(header, program, results_pretty):
+                path = easygui.filesavebox("Save Results", program, program + "_" + keyword, results_pretty)
+                if path is not None:
+                    save_file(path, results)
+                    exit(0)
+                else:
+                    exit(0)
             else:
                 exit(0)
-
-
-
 
     except SystemExit:
         exit(0)
     except:
-        easygui.exceptionbox("Error occurred. Please contact the maintainer of this program", program)
+        easygui.exceptionbox("Error occurred. Please contact the maintainer of this program.", program)
 
 
 main()
